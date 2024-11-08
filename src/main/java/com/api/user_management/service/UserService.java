ca3ef975d1e88d698698ad30054120bc1fc85d0f
Brhane Teamrat Gidey <bretama99@gmail.com>
2024-11-08 09:17:55 +0100

    UP-70 feat: Signup Input Validation

--- src/main/java/com/api/user_management/service/UserService.java
+++ src/main/java/com/api/user_management/service/UserService.java
@@ -6,11 +6,15 @@ import java.util.List;
 import javax.mail.MessagingException;
 import javax.mail.internet.AddressException;
 
-import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
 import com.api.user_management.shared.dto.UserDto;
+import com.api.user_management.ui.model.request.EmailVerificationRequestModel;
 import com.api.user_management.ui.model.request.ResetPasswordRequestModel;
-public interface UserService extends UserDetailsService{
+
+public interface UserService {
+
 	UserDto createUser(UserDto user) throws AddressException, MessagingException, IOException;
 	UserDto getuser(String email);
 	UserDto getUserByUserId(String userId);
@@ -22,11 +26,13 @@ public interface UserService extends UserDetailsService{
 	void deleteUser(String userId);
 	
 	//Get All Service
-	List<UserDto> getUsers(int page, int limit, String userType) throws AddressException, MessagingException, IOException;
 	UserDto updateUserStatus(String id, UserDto userDto);
 	String checkEmail(String email);
 	String resetPassword(ResetPasswordRequestModel resetPasswordDetail);
 	String changeAccountPassword(ResetPasswordRequestModel changePassRequest);
+	UserDetails loadUserByUsername(String email) throws UsernameNotFoundException;
+	List<UserDto> getUsers(int page, int limit, String userType, String searchKey, boolean isPublic, String department, String role) throws IOException;
+	String verifyEmail(EmailVerificationRequestModel requestModel);
+	String resendCode(EmailVerificationRequestModel requestModel);
 
-	
 }
