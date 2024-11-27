511c6611964624901e0a802871d901ddd9443ad0
Brhane Teamrat Gidey <bretama99@gmail.com>
2024-11-27 08:33:52 +0100

    UP-91 feat: Implement API endpoint to send messaeg

---
+++ src/main/java/com/api/user_management/controller/MessageController.java
@@ -0,0 +1,84 @@
+package com.api.user_management.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.api.user_management.service.MessageService;
+import com.api.user_management.ui.model.request.MessageRequestModel;
+import com.api.user_management.ui.model.response.MessageResponseModel;
+
+@RestController
+@RequestMapping("message")
+public class MessageController {
+
+	@Autowired
+	MessageService messagesService;
+	
+	@PostMapping
+	public MessageResponseModel saveMessages(@RequestBody MessageRequestModel messagesDetail) {
+		
+		MessageResponseModel returnValue = messagesService.addMessage(messagesDetail);
+		
+		return returnValue;
+		
+	}
+	@PutMapping(path = "/{messageId}")
+	public MessageResponseModel updateMessages(@RequestBody MessageRequestModel messageDetails, @PathVariable Long messageId) {
+		MessageResponseModel returnValue = messagesService.updateMessage(messageId, messageDetails);
+		return returnValue;
+	}
+	
+	@GetMapping(path = "/{messageId}")
+	public MessageResponseModel getMessage(@PathVariable Long messageId) {
+		MessageResponseModel returnValue = messagesService.getMessage(messageId);
+		return returnValue;
+	}
+	
+	@GetMapping
+	public List<MessageResponseModel> getMessages(
+			@RequestParam(value="searchKey", defaultValue = "") String searchKey, 
+			@RequestParam(value="page", defaultValue = "1") int page,
+			@RequestParam(value="limit", defaultValue = "1000") int limit,
+			@RequestParam(value="senderId", defaultValue = "0") Long senderId,			
+			@RequestParam(value="recieverId", defaultValue = "0") Long recieverId
+			) {
+		List<MessageResponseModel> returnValue = messagesService.getMessages(searchKey,page,limit,senderId, recieverId);
+		return returnValue;
+	}
+
+	@DeleteMapping(path = "/{messageId}")
+	public String deleteMessage(@PathVariable Long messageId) {
+		String returnValue = messagesService.deleteMessage(messageId);
+	    return returnValue;
+	}
+	
+	@GetMapping(path = "/chat-room")
+	public List<MessageResponseModel> getChatRoomMessages(
+			@RequestParam(value="searchKey", defaultValue = "") String searchKey, 
+			@RequestParam(value="page", defaultValue = "1") int page,
+			@RequestParam(value="limit", defaultValue = "1000") int limit,
+			@RequestParam(value="userId", defaultValue = "0") Long userId) {
+		List<MessageResponseModel> returnValue = messagesService.getChatRoomMessages(searchKey,page,limit,userId);
+		return returnValue;
+	}
+	
+	@PostMapping(path = "/chat-room")
+	public MessageResponseModel saveChatRoomMessages(@RequestBody MessageRequestModel messagesDetail) {
+		
+		MessageResponseModel returnValue = messagesService.saveChatRoomMessages(messagesDetail);
+		
+		return returnValue;
+		
+	}	
+	
+}
