ee5a060f9a2c9e2c0063c4339886dfb68eec895a
Brhane Teamrat Gidey <bretama99@gmail.com>
2024-11-30 13:29:16 +0100

    UP-94 feat: Implement Websocket event for new messages

--- src/main/java/com/api/user_management/controller/ChatController.java
+++ src/main/java/com/api/user_management/controller/ChatController.java
@@ -1,29 +1,47 @@
 package com.api.user_management.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.messaging.handler.annotation.MessageMapping;
-import org.springframework.messaging.handler.annotation.SendTo;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
-import com.api.user_management.io.entity.MessageEntity;
-import com.api.user_management.io.repositories.MessageRepository;
+import com.api.user_management.io.entity.ChatMessage;
+import com.api.user_management.io.entity.ChatRoom;
+import com.api.user_management.service.Impl.ChatMessageService;
+import com.api.user_management.shared.ChatNotification;
 
 import java.util.List;
 
-@RestController
+@Controller
+@RequiredArgsConstructor
 public class ChatController {
 
-    @Autowired
-    private MessageRepository messageRepository;
-
-    @GetMapping("/messages")
-    public List<MessageEntity> getMessages() {
-        return messageRepository.findAll();
-    }
+    private SimpMessagingTemplate messagingTemplate;
+    private ChatMessageService chatMessageService;
 
     @MessageMapping("/chat")
-    @SendTo("/topic/messages")
-    public MessageEntity sendMessage(MessageEntity message) {
-        return messageRepository.save(message);
+    public void processMessage(@Payload ChatMessage chatMessage) {
+        ChatMessage savedMsg = chatMessageService.save(chatMessage);
+        messagingTemplate.convertAndSendToUser(
+                chatMessage.getRecipientId(), "/queue/messages",
+                new ChatNotification(
+                        savedMsg.getId(),
+                        savedMsg.getSenderId(),
+                        savedMsg.getRecipientId(),
+                        savedMsg.getContent()
+                )
+        );
+    }
+
+    @GetMapping("/messages/{senderId}/{recipientId}")
+    public ResponseEntity<List<ChatMessage>> findChatMessages(@PathVariable String senderId,
+                                                 @PathVariable String recipientId) {
+        return ResponseEntity
+                .ok(chatMessageService.findChatMessages(senderId, recipientId));
+   
     }
 }
