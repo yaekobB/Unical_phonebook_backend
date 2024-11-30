6ad10465c139cc16d46bed42c3a215ee2b979374
Brhane Teamrat Gidey <bretama99@gmail.com>
2024-11-30 13:28:04 +0100

    UP-93 feat: Configure Websocket Server

---
+++ src/main/java/com/api/user_management/service/Impl/ChatMessageService.java
@@ -0,0 +1,32 @@
+package com.api.user_management.service.Impl;
+import com.api.user_management.io.entity.ChatMessage;
+import com.api.user_management.io.entity.ChatRoom;
+import com.api.user_management.io.repositories.ChatMessageRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class ChatMessageService {
+	
+	
+    private ChatMessageRepository repository;
+    private ChatRoomService chatRoomService;
+
+    public ChatMessage save(ChatMessage chatMessage) {
+        Optional<String> chatId = chatRoomService
+                .getChatRoomId(chatMessage.getSenderId(), chatMessage.getRecipientId(), true)
+                ; // You can create your own dedicated exception
+        chatMessage.setChatId(chatId.get());
+        repository.save(chatMessage);
+        return chatMessage;
+    }
+
+    public List<ChatMessage> findChatMessages(String senderId, String recipientId) {
+        Optional<String> chatId = chatRoomService.getChatRoomId(senderId, recipientId, false);
+        return chatId.map(repository::findByChatId).orElse(new ArrayList<>());
+    }
+}
